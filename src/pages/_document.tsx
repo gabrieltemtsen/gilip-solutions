2ae940cc83a85d0f6aea37ab1a1db8e022c44cfb
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-11-02 18:10:14 +0100

    updated meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+       <Head>
+        <meta name="description" content="Gilip Solutions " key="desc" />
+        <meta property="og:title" content="Innovative Tech Solutions" />
+        <meta
+          property="og:description"
+          content="Leading provider of innovative technology solutions for modern businesses"
+        />
+        <meta
+          property="og:image"
+          content="/G.png"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
